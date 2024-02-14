4ae55dd0af14cfe7c2a67c1e71ab5bc5ed201435
Muhammad Hamza <mh408800@gmail.com>
2024-02-14 13:08:06 +0500

    middlewares

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 // middlewares
-
-
-
-
-
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // server
-app.listen(process.env.PORT, ()=>{
-    console.log(`server running at http://localhost${process.env.PORT}/`)
+app.listen(process.env.PORT, () => {
+  console.log(`server running at http://localhost${process.env.PORT}/`);
 });
