13d4a0a7b9704e86c8576f11650f313acfcd768a
Muhammad Hamza <mh408800@gmail.com>
2024-02-14 16:54:09 +0500

    finalized Qr saved as image

--- controllers/qrController.js
+++ controllers/qrController.js
@@ -1,9 +1,10 @@
 import qr from "qrcode";
 import qrcodeTerminal from "qrcode-terminal";
+import fs from "fs";
+import path from "path";
 
 const GenerateQRcode = async (req, res) => {
-  // static link
-  const link = "https://github.com/muhammad-hamza-liaqat";
+  const { link } = req.body;
 
   if (!link) {
     return res
@@ -12,46 +13,36 @@ const GenerateQRcode = async (req, res) => {
   }
 
   try {
-    // Generate QR code
     const qrImage = await qr.toDataURL(link);
+    console.log("QR Code:");
 
-    // display the qr code in the terminal as well
+    // Display the qr code in the terminal
     qrcodeTerminal.generate(link, { small: true });
-    // format, how the Qr code will be presented in the browser
-    const htmlResponse = `
-            <html>
-            <head>
-                <title>QR Code</title>
-                <style>
-                    body {
-                        background-color: #2874A6; 
-                        display: flex;
-                        justify-content: center; 
-                        align-items: center; 
-                        height: 100vh; 
-                        margin: 0; 
-                    }
-                    .qr-code-container {
-                        text-align: center; 
-                        border-radius: 35px; 
-                        padding: 20px; 
-                        background-color: #fff; 
-                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-                    }
-                    .qr-code-container img {
-                        border-radius: 10; 
-                    }
-                </style>
-            </head>
-            <body>
-                <div class="qr-code-container">
-                    <h1>QR Code</h1>
-                    <img src="${qrImage}" alt="QR Code">
-                </div>
-            </body>
-            </html>
-        `;
-    res.status(200).send(htmlResponse);
+
+    // Save QR code image to uploads directory
+    const currentDir = process.cwd();  // getting current directory
+    const uploadsDir = path.join(currentDir, "uploads"); // searching if uploads directory found
+    // if directory "uploads" don't exist then it will create the directory
+    if (!fs.existsSync(uploadsDir)) {
+      fs.mkdirSync(uploadsDir);
+    }
+
+    // Generate filename with date and time prefix
+    const dateTimePrefix = new Date().toISOString().replace(/[:.]/g, "-");
+    const qrImageFilename = `qrCode_${dateTimePrefix}.png`;
+
+    const qrImagePath = path.join(uploadsDir, qrImageFilename);
+    const qrImageBuffer = Buffer.from(qrImage.split(",")[1], "base64");
+    fs.writeFileSync(qrImagePath, qrImageBuffer);
+
+    // Construct response with QR image and link
+    const response = {
+      message: "QR code generated",
+      qrImage: qrImagePath,
+      link: link
+    };
+
+    res.status(200).json(response);
   } catch (err) {
     console.error("Error generating QR code:", err);
     res.status(500).json({ status: 500, error: "Internal server error" });
